708fc4a3a84905d894a2e9771b128866c0707ace
markdnolan <mark.d.nolan@icloud.com>
2023-08-16 14:08:04 -0400

    Initial commit

---
+++ Hello-cpp/Hello.cpp
@@ -0,0 +1,13 @@
+//
+//  Hello.cpp
+//  teach-yourself-cpp
+//
+//  Created by Mark Nolan on 8/15/23.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
---
+++ lesson1-quiz/lesson1-quiz2.cpp
@@ -0,0 +1,17 @@
+//
+//  lesson1-quiz2.cpp
+//  teach-yourself-cpp
+//
+//  Created by Mark Nolan on 8/16/23.
+//
+
+#include <iostream>
+int main() {
+    int x = 8;
+    int y = 6;
+    std::cout << std::endl;
+    std::cout << x - y << " " << x * y << " " << x + y;
+    std::cout << std::endl;
+    return 0;
+}
+
