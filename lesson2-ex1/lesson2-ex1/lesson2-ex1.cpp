8fc3934a9100c0d4e41b78f240976b94ec697832
markdnolan <mark.d.nolan@icloud.com>
2023-08-18 12:59:17 -0400

    lessong 2

---
+++ lesson2-ex1/lesson2-ex1/lesson2-ex1.cpp
@@ -0,0 +1,13 @@
+//
+//  lesson2-ex1.cpp
+//  lesson2-ex1
+//
+//  Created by Mark Nolan on 8/18/23.
+//
+
+#include<iostream>
+
+int main(){
+    std::cout << "Is there a bug here?" << std::endl;
+    return(0);
+}
